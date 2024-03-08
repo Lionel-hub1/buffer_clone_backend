c7efd64b96e8da9b19918e7ca86d27eed1228516
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-08 16:52:06 +0200

    Add User model field and Type and Post models

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
 
 
 class User(AbstractBaseUser):
@@ -12,6 +13,8 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
 
+    USERNAME_FIELD = 'email'
+
     def __str__(self):
         return f'{self.username} - {self.email}'
 
@@ -20,3 +23,23 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- buffer_clone_backend/urls.py
+++ buffer_clone_backend/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+"""
+#### Steps For Creating API ####
+
+Step 1: Create Model (For Creating database )
+Step 2: Create Serializer (For converting model to JSON)
+Step 3: Create View (For creating API)
+Step 4: Create URL (For mapping the API / Creating the endpoint)
+"""
