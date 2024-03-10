d9cdada41f2a2f675d7f9e5762fa81c5dc2dc47d
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-10 23:05:28 +0200

    Add CORS headers and update installed apps in settings.py

--- buffer_clone_backend/settings.py
+++ buffer_clone_backend/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'rest_framework',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:5173",
+    "http://localhost:5174"
+]
